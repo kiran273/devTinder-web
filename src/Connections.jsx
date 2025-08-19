401c2912e969af5838cd4bbd43bb67a993a9f1ba
gkkumar <gkkumar7416@gmail.com>
2025-08-20 01:01:03 +0530

    accepted/rejected connection requests

--- src/Connections.jsx
+++ src/Connections.jsx
@@ -25,7 +25,8 @@ const Connections = () => {
 
   if (!connections) return;
 
-  if (connections.length === 0) return <h1> No Connections Found</h1>;
+  if (connections.length === 0)
+    return <h1 className="flex justify-center my-10"> No Connections Found</h1>;
 
   return (
     <div className="text-center my-10">
@@ -41,7 +42,7 @@ const Connections = () => {
           >
             <div>
               <img
-                className="rounded-full w-20 h-20"
+                className="object-cover rounded-full w-20 h-15"
                 alt="photo"
                 src={photoUrl}
               />
